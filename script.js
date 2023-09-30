3abaa0288617bd3fbf40668e6d421b2c703e07a3
ShraddhaNandPandey <shraddhanandpandey1729@gmail.com>
2023-09-30 22:03:18 +0530

    Added menu hamburger

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ var swiper = new Swiper(".mySwiper", {
       
     },
   });
+
+ 
+
+  var sideMenu = document.getElementById('side-menu');
+  function openMenu() {
+      sideMenu.classList.remove('left-[-250px]');
+      sideMenu.classList.add('left-0');
+  }
+
+  function closeMenu() {
+      sideMenu.classList.remove('left-0');
+      sideMenu.classList.add('left-[-250px]');
+  }
